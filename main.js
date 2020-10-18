5f817427bd4e354fe217962e6ea7d48f0e784274
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-18 12:00:36 +0200

    download p element on js with class

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 const btnAwesome = document.querySelector("button1");
 const btnAmazing = document.querySelector("button2");
 const btnAttention = document.querySelector("button3");
+
+const textEle = document.querySelector("squares");
