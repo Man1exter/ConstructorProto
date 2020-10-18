640265f42d71f9d095c78e7d36296e89952736b3
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-18 12:16:23 +0200

    completee prototype with constructor exercises

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const btnAwesome = document.querySelector("button1");
-const btnAmazing = document.querySelector("button2");
-const btnAttention = document.querySelector("button3");
+const btnAwesome = document.querySelector(".button1");
+const btnAmazing = document.querySelector(".button2");
+const btnAttention = document.querySelector(".button3");
 
-const textEle = document.querySelector("squares");
+const textEle = document.querySelector(".squares");
 
 function Menu(name,price){
     this.name = name;
@@ -12,14 +12,14 @@ Menu.prototype.dishes = function () {
 textEle.innerText = `This special is ${this.name} with price ${this.price}`;
 }
 
-const newDish = new Menu('Pizza',25);
+const newDish1 = new Menu('Pizza',25);
 const newDish2 = new Menu('Burger',12);
 const newDish3 = new Menu('Nuggets',10);
 
 
 
 btnAmazing.addEventListener("click", function (){
-    newDish.dishes();
+    newDish1.dishes();
 })
 btnAwesome.addEventListener("click", function(){
     newDish2.dishes();
