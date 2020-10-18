50d820f72e0f16a9ec358e6ad4f29086ab835025
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-18 12:06:14 +0200

    ADD callback on prototype with functions cnt

--- main.js
+++ main.js
@@ -8,7 +8,14 @@ function Menu(name,price){
     this.name = name;
     this.price = price;
 }
+Menu.prototype.dishes = function () {
+
+}
 
 const newDish = new Menu('Pizza',25);
 const newDish2 = new Menu('Burger',12);
 const newDish3 = new Menu('Nuggets',10);
+
+newDish.dishes();
+newDish2.dishes();
+newDish3.dishes();
