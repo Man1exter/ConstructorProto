cb087e560df4301c1336b2ee503cec6ac1d83ad8
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-18 12:03:58 +0200

    construct with constructor dishes with them nm/pr

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ const btnAmazing = document.querySelector("button2");
 const btnAttention = document.querySelector("button3");
 
 const textEle = document.querySelector("squares");
+
+function Menu(name,price){
+    this.name = name;
+    this.price = price;
+}
+
+const newDish = new Menu('Pizza',25);
+const newDish2 = new Menu('Burger',12);
+const newDish3 = new Menu('Nuggets',10);
