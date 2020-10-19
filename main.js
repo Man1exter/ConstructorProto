f28261404f26c23798b15e619e63b42eb875a95e
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-19 09:20:56 +0200

    uncommend first function to three buttons and comm

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-// const btnAwesome = document.querySelector(".button1");
-// const btnAmazing = document.querySelector(".button2");
-// const btnAttention = document.querySelector(".button3");
+const btnAwesome = document.querySelector(".button1");
+const btnAmazing = document.querySelector(".button2");
+const btnAttention = document.querySelector(".button3");
 
-// const textEle = document.querySelector(".squares");
+const textEle = document.querySelector(".squares");
 
-// function Menu(name,price){
-//     this.name = name;
-//     this.price = price;
-// }
-// Menu.prototype.dishes = function () {
-// textEle.innerText = `This special is ${this.name} with price ${this.price}`;
-// }
+function Menu(name,price){
+    this.name = name;
+    this.price = price;
+}
+Menu.prototype.dishes = function () {
+textEle.innerText = `This special is ${this.name} with price ${this.price}`;
+}
 
-// const newDish1 = new Menu('Pizza',25);
-// const newDish2 = new Menu('Burger',12);
-// const newDish3 = new Menu('Nuggets',10);
-
-
-
-// btnAmazing.addEventListener("click", function (){
-//     newDish1.dishes();
-// })
-// btnAwesome.addEventListener("click", function(){
-//     newDish2.dishes();
-// });
-// btnAttention.addEventListener("click", function () {
-//     newDish3.dishes();
-// });
+const newDish1 = new Menu('Pizza',25);
+const newDish2 = new Menu('Burger',12);
+const newDish3 = new Menu('Nuggets',10);
 
 
 
+btnAmazing.addEventListener("click", function (){
+    newDish1.dishes();
+})
+btnAwesome.addEventListener("click", function(){
+    newDish2.dishes();
+});
+btnAttention.addEventListener("click", function () {
+    newDish3.dishes();
+});
+
+
+////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////////
 // const user = {
