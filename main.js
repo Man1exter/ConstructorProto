50377adca1a4088f48770dfd4ab16e0288d91c36
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-19 09:20:26 +0200

    comment exercises and get done them

--- main.js
+++ main.js
@@ -1,49 +1,54 @@
-const btnAwesome = document.querySelector(".button1");
-const btnAmazing = document.querySelector(".button2");
-const btnAttention = document.querySelector(".button3");
+// const btnAwesome = document.querySelector(".button1");
+// const btnAmazing = document.querySelector(".button2");
+// const btnAttention = document.querySelector(".button3");
 
-const textEle = document.querySelector(".squares");
+// const textEle = document.querySelector(".squares");
 
-function Menu(name,price){
-    this.name = name;
-    this.price = price;
-}
-Menu.prototype.dishes = function () {
-textEle.innerText = `This special is ${this.name} with price ${this.price}`;
-}
+// function Menu(name,price){
+//     this.name = name;
+//     this.price = price;
+// }
+// Menu.prototype.dishes = function () {
+// textEle.innerText = `This special is ${this.name} with price ${this.price}`;
+// }
 
-const newDish1 = new Menu('Pizza',25);
-const newDish2 = new Menu('Burger',12);
-const newDish3 = new Menu('Nuggets',10);
+// const newDish1 = new Menu('Pizza',25);
+// const newDish2 = new Menu('Burger',12);
+// const newDish3 = new Menu('Nuggets',10);
 
 
 
-btnAmazing.addEventListener("click", function (){
-    newDish1.dishes();
-})
-btnAwesome.addEventListener("click", function(){
-    newDish2.dishes();
-});
-btnAttention.addEventListener("click", function () {
-    newDish3.dishes();
-});
+// btnAmazing.addEventListener("click", function (){
+//     newDish1.dishes();
+// })
+// btnAwesome.addEventListener("click", function(){
+//     newDish2.dishes();
+// });
+// btnAttention.addEventListener("click", function () {
+//     newDish3.dishes();
+// });
 
 
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////////
-const user = {
-    name: 'Mariusz',
-    age: 19,
-    'fav-color': 'black',
-    car : {
-        brand: 'Audi'
-    }
-}
-function show () {
-console.log(`${this.name} had ${this.age} his car had color ${this.fav-color}`);
-}
+// const user = {
+//     name: 'Mariusz',
+//     age: 19,
+//     'fav-color': 'black',
+//     car : {
+//         brand: 'Audi'
+//     }
+// }
 
-function show2 () {
-    
-}
+// function showX () {
+// console.log(`${this.name} had ${this.age} his car had color ${this['fav-color']}`);
+// };
+
+
+// const show2 = function (color) {
+// console.log(`His brand is ${this.car.brand} and had color ${color}`);
+// };
+
+// showX.bind(user)();
+// show2.call(user, 'dark');
